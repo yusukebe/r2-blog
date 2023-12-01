b335f4513da43219f067a108791a1e3d3960f533
Yusuke Wada <yusuke@kamawada.com>
2023-12-01 18:11:12 +0900

    fix: handle `command`

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,32 @@ import { getEnv } from '@hono/vite-dev-server/cloudflare-pages'
 import pages from '@hono/vite-cloudflare-pages'
 import { local2r2 } from './plugins/local2r2'
 
-export default defineConfig({
-  ssr: {
-    external: ['micromark']
-  },
-  plugins: [
-    pages({
-      minify: true
-    }),
-    local2r2({
-      r2Buckets: ['BUCKET'],
-      dir: './contents'
-    }),
-    devServer({
-      entry: 'src/index.tsx',
-      env: getEnv({
+export default defineConfig(({ command }) => {
+  if (command === 'build') {
+    return {
+      plugins: [
+        pages({
+          minify: true
+        })
+      ]
+    }
+  }
+  return {
+    ssr: {
+      external: ['micromark']
+    },
+    plugins: [
+      local2r2({
         r2Buckets: ['BUCKET'],
-        r2Persist: true
+        dir: './contents'
+      }),
+      devServer({
+        entry: 'src/index.tsx',
+        env: getEnv({
+          r2Buckets: ['BUCKET'],
+          r2Persist: true
+        })
       })
-    })
-  ]
+    ]
+  }
 })
